80c443017de591e1a3a8a107ccb7eb49ed83a02b
KarthickDamodaran <57034379+KarthickDamodaran@users.noreply.github.com>
2019-12-07 16:56:28 +0530

    Add files via upload

---
+++ neural_networks_mt2018509.s
@@ -0,0 +1,367 @@
+;IMPORTANT: Files needed for running: neural_networks.s and printmsg.c only     
+	 AREA     appcode, CODE, READONLY
+     EXPORT __main
+		IMPORT printMsg
+	 IMPORT printMsg2p
+	 IMPORT printMsg4p 
+     ENTRY
+;Registers s20,s21 and s22 hold the input bits a,b and c respectively
+;For a not gate, only inputs a and c are used to calculate the argument to the sigmoid function. b is ignored.
+;For a given input vector, the output will be printed on the terminal
+__main  FUNCTION
+	 VLDR.F32 s20,=1;input a in s20
+	 VLDR.F32 s21,=0;input b in s21
+	 VLDR.F32 s22,=0;input c in s22
+	 MOV r5,#0;To have input combinatons count
+	 MOV r6,#1
+	 VCVT.U32.F32 s23,s20;conversion for printing and moving 'a' to s23
+	 VCVT.U32.F32 s24,s21;conversion for printing and moving 'b' to s24
+	 VCVT.U32.F32 s25,s22;conversion for printing and moving 'c' to s25
+	 
+	 VLDR.F32 s29,=0.5;To decide logic values depending on output from neural network. If output>0.5 considered logic 1, else a logic 0
+	 ;B and1;Branches to and gate neural network
+	 ;ret routine takes care of changing inputs and calling AND gate
+	 ;ret1 routine takes care of changing inputs and calling OR gate
+	 ;ret2 routine takes care of changing inputs and calling NOT gate
+	 ;ret3 routine takes care of changing inputs and calling NAND gate
+	 ;ret4 routine takes care of changing inputs and calling NOR gate
+ret	 CMP r5,#0
+     BEQ input0
+     ;ADD r5,r5,r6
+     CMP r5,#1
+	 BEQ input1
+     CMP r5,#2
+	 BEQ input2
+     CMP r5,#3
+ 	 BEQ input3
+	 CMP r5,#4
+	 MOV r5,#0	 ;if all input combinations exhausted, reset r5 and move to the next gate
+     BEQ ret1
+;0th input combination input0
+;1st is input1
+;2nd is input2
+;3rd is input3
+;The input combination count r5 is incremented in the "check" routine
+input0 VLDR.F32 s21,=0;input b in s21
+	   VLDR.F32 s22,=0;input c in s22
+	   VCVT.U32.F32 s24,s21;conversion for printing and moving 'b' to s24
+	   VCVT.U32.F32 s25,s22;conversion for printing and moving 'c' to s25
+	   MOV r0,#0;r0 used as an identifier for the logic gate to print the logic gate name in printMsg function
+	   BL printMsg;called to print the logic gate name and input output names
+	   B and1
+
+input1 VLDR.F32 s21,=0
+       VLDR.F32 s22,=1
+	   VCVT.U32.F32 s24,s21;conversion for printing and moving 'b' to s24
+	   VCVT.U32.F32 s25,s22;conversion for printing and moving 'c' to s25
+	   B and1
+	 
+input2 VLDR.F32 s21,=1
+       VLDR.F32 s22,=0
+	   VCVT.U32.F32 s24,s21;conversion for printing and moving 'b' to s24
+	   VCVT.U32.F32 s25,s22;conversion for printing and moving 'c' to s25
+	   B and1
+	   
+input3 VLDR.F32 s21,=1
+       VLDR.F32 s22,=1
+	   VCVT.U32.F32 s24,s21;conversion for printing and moving 'b' to s24
+	   VCVT.U32.F32 s25,s22;conversion for printing and moving 'c' to s25
+	   B and1
+;ret1 responsible to changing inputs and calling and gate
+ret1 
+     CMP r5,#0
+     BEQ input0a
+     ;ADD r5,r5,r6
+     CMP r5,#1
+	 BEQ input1a
+     CMP r5,#2
+	 BEQ input2a
+     CMP r5,#3
+ 	 BEQ input3a
+	 CMP r5,#4
+	 MOV r5,#0
+     BEQ ret2
+	 
+input0a VLDR.F32 s21,=0;input b in s21
+	   VLDR.F32 s22,=0;input c in s22
+	   VCVT.U32.F32 s24,s21;conversion for printing and moving 'b' to s24
+	   VCVT.U32.F32 s25,s22;conversion for printing and moving 'c' to s25
+	   MOV r0,#1;r0 used as an identifier for the logic gate to print the logic gate name in printMsg function
+	   BL printMsg;called to print the logic gate name and input output names
+	   B or
+
+input1a VLDR.F32 s21,=0
+       VLDR.F32 s22,=1
+	   VCVT.U32.F32 s24,s21;conversion for printing and moving 'b' to s24
+	   VCVT.U32.F32 s25,s22;conversion for printing and moving 'c' to s25
+	   B or
+	 
+input2a VLDR.F32 s21,=1
+       VLDR.F32 s22,=0
+	   VCVT.U32.F32 s24,s21;conversion for printing and moving 'b' to s24
+	   VCVT.U32.F32 s25,s22;conversion for printing and moving 'c' to s25
+	   B or
+	   
+input3a VLDR.F32 s21,=1
+       VLDR.F32 s22,=1
+	   VCVT.U32.F32 s24,s21;conversion for printing and moving 'b' to s24
+	   VCVT.U32.F32 s25,s22;conversion for printing and moving 'c' to s25
+	   B or
+
+;ret2 responsible to changing inputs and calling not gate
+ret2  
+     CMP r5,#0
+     BEQ input0b
+     ;ADD r5,r5,r6
+     CMP r5,#1
+	 BEQ input1b
+     CMP r5,#2
+	 MOV r5,#0
+	 BEQ ret3
+     	 
+input0b VLDR.F32 s21,=0;input b in s21
+	   VLDR.F32 s22,=0;input c in s22
+	   VCVT.U32.F32 s24,s21;conversion for printing and moving 'b' to s24
+	   VCVT.U32.F32 s25,s22;conversion for printing and moving 'c' to s25
+	   MOV r0,#2;r0 used as an identifier for the logic gate to print the logic gate name in printMsg function
+	   BL printMsg;called to print the logic gate name and input output names
+	   B not
+
+input1b VLDR.F32 s21,=0
+       VLDR.F32 s22,=1
+	   VCVT.U32.F32 s24,s21;conversion for printing and moving 'b' to s24
+	   VCVT.U32.F32 s25,s22;conversion for printing and moving 'c' to s25
+	   B not
+;ret3 responsible to changing inputs and calling nand gate
+ret3 CMP r5,#0
+     BEQ input0c
+     ;ADD r5,r5,r6
+     CMP r5,#1
+	 BEQ input1c
+     CMP r5,#2
+	 BEQ input2c
+     CMP r5,#3
+ 	 BEQ input3c
+	 CMP r5,#4
+	 MOV r5,#0
+     BEQ ret4
+	 
+input0c VLDR.F32 s21,=0;input b in s21
+	   VLDR.F32 s22,=0;input c in s22
+	   VCVT.U32.F32 s24,s21;conversion for printing and moving 'b' to s24
+	   VCVT.U32.F32 s25,s22;conversion for printing and moving 'c' to s25
+	   MOV r0,#3;r0 used as an identifier for the logic gate to print the logic gate name in printMsg function
+	   BL printMsg;called to print the logic gate name and input output names
+	   B nand
+
+input1c VLDR.F32 s21,=0
+       VLDR.F32 s22,=1
+	   VCVT.U32.F32 s24,s21;conversion for printing and moving 'b' to s24
+	   VCVT.U32.F32 s25,s22;conversion for printing and moving 'c' to s25
+	   B nand
+	 
+input2c VLDR.F32 s21,=1
+       VLDR.F32 s22,=0
+	   VCVT.U32.F32 s24,s21;conversion for printing and moving 'b' to s24
+	   VCVT.U32.F32 s25,s22;conversion for printing and moving 'c' to s25
+	   B nand
+	   
+input3c VLDR.F32 s21,=1
+       VLDR.F32 s22,=1
+	   VCVT.U32.F32 s24,s21;conversion for printing and moving 'b' to s24
+	   VCVT.U32.F32 s25,s22;conversion for printing and moving 'c' to s25
+	   B nand
+;ret4 responsible to changing inputs and calling nor gate
+ret4 CMP r5,#0
+     BEQ input0d
+     ;ADD r5,r5,r6
+     CMP r5,#1
+	 BEQ input1d
+     CMP r5,#2
+	 BEQ input2d
+     CMP r5,#3
+ 	 BEQ input3d
+	 CMP r5,#4
+	 MOV r5,#0;if all input combinations exhausted, halt the program execution
+     BEQ stop
+	 
+input0d VLDR.F32 s21,=0;input b in s21
+	   VLDR.F32 s22,=0;input c in s22
+	   VCVT.U32.F32 s24,s21;conversion for printing and moving 'b' to s24
+	   VCVT.U32.F32 s25,s22;conversion for printing and moving 'c' to s25
+	   MOV r0,#4;r0 used as an identifier for the logic gate to print the logic gate name in printMsg function
+	   BL printMsg;called to print the logic gate name and input output names
+	   B nor
+
+input1d VLDR.F32 s21,=0
+       VLDR.F32 s22,=1
+	   VCVT.U32.F32 s24,s21;conversion for printing and moving 'b' to s24
+	   VCVT.U32.F32 s25,s22;conversion for printing and moving 'c' to s25
+	   B nor
+	 
+input2d VLDR.F32 s21,=1
+       VLDR.F32 s22,=0
+	   VCVT.U32.F32 s24,s21;conversion for printing and moving 'b' to s24
+	   VCVT.U32.F32 s25,s22;conversion for printing and moving 'c' to s25
+	   B nor
+	   
+input3d VLDR.F32 s21,=1
+       VLDR.F32 s22,=1
+	   VCVT.U32.F32 s24,s21;conversion for printing and moving 'b' to s24
+	   VCVT.U32.F32 s25,s22;conversion for printing and moving 'c' to s25
+	   B nor;Branches to nor gate neural network
+
+
+stop B stop;Halts
+
+;subroutine check1 is used to assign a logic 1 or a logic 0 to the final output, depending on the output from the sigmoid function
+check  ADD r5,r5,r6;r5 incremented to move to the next input combination
+       VCMP.F32 s12,s29 ;checking if s12>0.5, s29 is initialised with 0.5
+	   vmrs    APSR_nzcv, FPSCR
+	   BGT one ;if s12>0.5 is true, branches to make final output as logic 1
+	   VLDR.F32 s4,=0;if s12>0.5 is false, final output is a logic 0
+check1 VCVT.U32.F32 s4,s4;conversion for printing. s4 has the final logic gate output value
+	   VMOV.F32 r0,s23;moving the value of 'a' into r0 to print
+	   VMOV.F32 r1,s24;moving the value of 'b' into r1 to print
+	   VMOV.F32 r2,s25;moving the value of 'c' into r2 to print
+	   VMOV.F32 r3,s4;moving the value of 'd' into r3 to print
+       BX lr;returns to the calling function. printmsg4p will be called after returning to print the above values
+
+one  VLDR.F32 s4,=1;if s12>0.5 is true, branches to make final output as logic 1
+     B check1;branches back to check1 to perform the further required things to print
+	 
+	 ;In the following code, registers s0,s1 & s2 hold the weights for inputs a,b & c respectively;s3 holds the bias
+;z=wa*a+wb*b+wc*c+bias is calculated and stored in s3 register
+and1 VLDR.F32 s0,=-0.1;wa
+     VLDR.F32 s1,=0.2;wb
+	 VLDR.F32 s2,=0.2;wc
+	 VLDR.F32 s3,=-0.2;bias
+	 VMUL.F32 s0,s0,s20;wa*a calculated
+	 VMUL.F32 s1,s1,s21;wb*b calculated
+	 VMUL.F32 s2,s2,s22;wc*c calculated
+	 VADD.F32 s0,s0,s1;wa*a+wb*b calculated
+	 VADD.F32 s0,s0,s2;wa*a+wb*b+wc*c calculated
+	 VADD.F32 s3,s0,s3;wa*a+wb*b+wc*c+bias is calculated
+	 BL calc;calling the subroutine to calculate the sigmoid function value;z is stored in s3 
+	 ;MOV r0,#0;r0 used as an identifier for the logic gate to print the logic gate name in printMsg function
+	 ;BL printMsg;called to print the logic gate name and input output names
+	 BL check;called to assign logic 1 or logic 0 to output
+	 BL printMsg4p;called to print the input output values
+	 B ret;returns to move to next gate
+	
+or   VLDR.F32 s0,=-0.1;wa
+     VLDR.F32 s1,=0.7;wb
+	 VLDR.F32 s2,=0.7;wc
+	 VLDR.F32 s3,=-0.1;bias
+	 VMUL.F32 s0,s0,s20
+	 VMUL.F32 s1,s1,s21
+	 VMUL.F32 s2,s2,s22
+	 VADD.F32 s0,s0,s1
+	 VADD.F32 s0,s0,s2
+	 VADD.F32 s3,s0,s3;z value
+	 BL calc;calling the subroutine to calculate the sigmoid function value;z is stored in s3
+	 MOV r0,#1;r0 used as an identifier for the logic gate to print the logic gate name in printMsg function
+	 ;BL printMsg;called to print the logic gate name and input output names
+	 BL check;called to assign logic 1 or logic 0 to output
+	 BL printMsg4p;called to print the input output values
+	 B ret1;returns to move to next gate
+	
+not  VLDR.F32 s0,=0.5;wa
+     VLDR.F32 s1,=0;wb
+	 VLDR.F32 s2,=-0.7
+	 VLDR.F32 s3,=0.1;bias
+	 VMUL.F32 s0,s0,s20
+	 VMUL.F32 s1,s1,s21
+	 VMUL.F32 s2,s2,s22
+	 VADD.F32 s0,s0,s1
+	 VADD.F32 s0,s0,s2
+	 VADD.F32 s3,s0,s3;z value
+	 BL calc;calling the subroutine to calculate the sigmoid function value;z is stored in s3
+	 MOV r0,#2;r0 used as an identifier for the logic gate to print the logic gate name in printMsg function
+	 ;BL printMsg;called to print the logic gate name and input output names
+	 BL check;called to assign logic 1 or logic 0 to output
+	 BL printMsg4p;called to print the input output values
+	 B ret2;returns to move to next gate
+	
+nand VLDR.F32 s0,=0.6;wa
+     VLDR.F32 s1,=-0.8;wb
+	 VLDR.F32 s2,=-0.8;wc
+     VLDR.F32 s3,=0.3;bias
+	 VMUL.F32 s0,s0,s20
+	 VMUL.F32 s1,s1,s21
+	 VMUL.F32 s2,s2,s22
+	 VADD.F32 s0,s0,s1
+	 VADD.F32 s0,s0,s2
+	 VADD.F32 s3,s0,s3;z value
+	 BL calc;calling the subroutine to calculate the sigmoid function value;z is stored in s3
+	 MOV r0,#3;r0 used as an identifier for the logic gate to print the logic gate name in printMsg function
+	 ;BL printMsg;called to print the logic gate name and input output names
+	 BL check;called to assign logic 1 or logic 0 to output
+	 BL printMsg4p;called to print the input output values
+	 B ret3;returns to move to next gate
+	 
+nor  VLDR.F32 s0,=0.5;wa
+     VLDR.F32 s1,=-0.7;wb
+	 VLDR.F32 s2,=-0.7;wc
+	 VLDR.F32 s3,=0.1;bias
+	 VMUL.F32 s0,s0,s20
+	 VMUL.F32 s1,s1,s21
+	 VMUL.F32 s2,s2,s22
+	 VADD.F32 s0,s0,s1
+	 VADD.F32 s0,s0,s2
+	 VADD.F32 s3,s0,s3;z value
+	 BL calc;calling the subroutine to calculate the sigmoid function value;z is stored in s3
+	 MOV r0,#4;r0 used as an identifier for the logic gate to print the logic gate name in printMsg function
+	 ;BL printMsg;called to print the logic gate name and input output names
+	 BL check;called to assign logic 1 or logic 0 to output
+	 BL printMsg4p;called to print the input output values
+	 B ret4;Branches to stop as all logic gates are done
+	 
+	 
+;This is the subroutine to calculate the sigmoid function value
+calc VLDR.F32 s6,=30;Number of series terms required in e^x series
+	 VLDR.F32 s5,=1
+	 VLDR.F32 s8,=0
+	 VLDR.F32 s9,=0
+	 VLDR.F32 s13,=-1;Used to multiply z by -1 and hence to calculate exp(-z)
+	 VMUL.F32 s3,s3,s13;(-z is stored in the s3 register)
+sign VLDR.F32 s7,=1; s7 holds the factorial result;initialised with a 1 as 0!=1
+     VMOV.F32 s10,s8; storing the 'n' value in s10 to retrieve it later. n denotes the iteration count and n>=0
+loop VCMP.F32 s8, #0   ; This loop computes the factorial and stores in s7 the factorial result
+     vmrs    APSR_nzcv, FPSCR
+     BEQ next; if the number 'n' to be computed factorial is a 0, s7 is undisturbed and the control jumps to calculate x^n
+     VMUL.F32 s7,s8,s7;if non zero 'n' s7=s8*s7
+     VSUB.F32 s8,s8,s5;s8=s8-1, decrementing s8 to repeat the loop
+     B loop; repeats the process till factorial is computed
+	 
+	 ;In the following discussion x=-z;
+	 
+next VMOV.F32 s8,s10;retreiving the current iteration count 'n' for x^n calculation
+     VLDR.F32 s5,=1;used for decrementing purposes
+	 VLDR.F32 s4,=1;holds x^n and is initialised to 1 as x^0=1
+	 VCMP.F32 s8,#0;if n=0,x^n=1 and hence s4 register which holds x^n initialised to 1 is undisturbed
+	 vmrs    APSR_nzcv, FPSCR
+	 BEQ new
+	 
+term VMUL.F32 s4,s4,s3;s4=s4*s3 is repeated n times, with s4=1 as initial value, we get s4=x^n  
+     VSUB.F32 s8,s8,s5;decrementing s8 to keep track of the count
+	 VCMP.F32 s8,#0
+	 vmrs    APSR_nzcv, FPSCR
+	 BGT term
+	 
+new  VDIV.F32 s4,s4,s7;Here,s4={(x^n)/n!} is performed i.e series terms are computed
+     VADD.F32 s9,s4,s9;s9 is initialised with a 0 and the series terms are accumulated in s9
+	 VMOV.F32 s8,s10;retreiving iteration count
+	 VADD.F32 s8,s8,s5;incrementing iteration count
+	 VCMP.F32 s8,s6;comparing it against the required series terms
+	 vmrs    APSR_nzcv, FPSCR
+	 BLT sign;if the required number of series terms not computed and accumulated, the computation of the next series term happens
+	 
+     ;BX lr
+	 ;the following routine uses the e^x i.e exp(-z) calculated above(which is available in s9) and computes the sigmoid function value
+func VLDR.F32 s11,=1
+     VADD.F32 s12,s11,s9;denominator = 1+exp(-z)
+	 VDIV.F32 s12,s11,s12;function value calculated 1/(1+exp(-z))
+	 BX lr;returns to the main function
+     ENDFUNC
+     END 
---
+++ printmsg_mt2018509.c
@@ -0,0 +1,148 @@
+
+#include "stm32f4xx.h"
+#include <string.h>
+void printMsg(const int a)
+{
+	 char Msg[100];
+	 char *ptr;
+	if(a==0)
+	{
+	 sprintf(Msg, "\nAnd Gate \na   b   c   y\n");
+	 ptr = Msg ;
+	}
+	if(a==1)
+	{
+	 sprintf(Msg, "\nOr Gate \na   b   c   y\n");
+	 ptr = Msg ;
+	}
+	if(a==2)
+	{
+	 sprintf(Msg, "\nNot Gate \na   b   c   y\n");
+	 ptr = Msg ;
+	}
+	if(a==3)
+	{
+	 sprintf(Msg, "\nNand Gate \na   b   c   y\n");
+	 ptr = Msg ;
+	}
+	if(a==4)
+	{
+	 sprintf(Msg, "\nNor Gate \na   b   c   y\n");
+	 ptr = Msg ;
+	}
+	
+	if(a==5)
+	{
+	 sprintf(Msg, "\nXnor Gate \na   b   c   y\n");
+	 ptr = Msg ;
+	}
+   while(*ptr != '\0'){
+      ITM_SendChar(*ptr);
+      ++ptr;
+   }
+}
+void printMsg2p(const int a, const int b)
+{
+	 char Msg[100];
+	 char *ptr;
+	 sprintf(Msg, "\n%d", a);
+	 ptr = Msg ;
+   while(*ptr != '\0')
+	 {
+      ITM_SendChar(*ptr);
+      ++ptr;
+   }
+	 sprintf(Msg, " %d", b);
+	 ptr = Msg ;
+   while(*ptr != '\0')
+	 {
+      ITM_SendChar(*ptr);
+      ++ptr;
+   }
+}
+
+void printMsg4p(const int a, const int b, const int c, const int d, const int e)
+{
+	 char Msg[100];
+	 char *ptr;
+	 // Printing the message
+	//sprintf(Msg,"\na   b   c   y\n");
+	 //ptr = Msg ;
+   //while(*ptr != '\0')
+	 //{
+    //  ITM_SendChar(*ptr);
+     // ++ptr;
+   //}
+	 //Printing the first parameter
+	 sprintf(Msg, "%d   ", a);
+	 ptr = Msg ;
+   while(*ptr != '\0')
+	 {
+      ITM_SendChar(*ptr);
+      ++ptr;
+   }
+	 // Printing the message
+	 //sprintf(Msg,"x1: ");
+	 //ptr = Msg ;
+   //while(*ptr != '\0')
+	 //{
+     // ITM_SendChar(*ptr);
+      //++ptr;
+   //}
+	 sprintf(Msg, "%d   ", b);
+	 ptr = Msg ;
+   while(*ptr != '\0')
+	 {
+      ITM_SendChar(*ptr);
+      ++ptr;
+   }
+	 // Printing the message
+	 /*sprintf(Msg,"x2: ");
+	 ptr = Msg ;
+   while(*ptr != '\0')
+	 {
+      ITM_SendChar(*ptr);
+      ++ptr;
+   }*/
+	 if(c!=2)
+	 {
+	 sprintf(Msg, "%d   ", c);
+	 ptr = Msg ;
+   while(*ptr != '\0')
+	 {
+      ITM_SendChar(*ptr);
+      ++ptr;
+   }
+ }
+	 // Printing the message
+	 /*sprintf(Msg,"output: ");
+	 ptr = Msg ;
+   while(*ptr != '\0')
+	 {
+      ITM_SendChar(*ptr);
+      ++ptr;
+   }*/
+	 sprintf(Msg, "%d\n", d);
+	 ptr = Msg ;
+   while(*ptr != '\0')
+	 {
+      ITM_SendChar(*ptr);
+      ++ptr;
+	 }
+
+	 // Printing the message
+	 /*sprintf(Msg,"\nPrinting Fifth parameter e (check this value is correct or not): ");
+	 ptr = Msg ;
+   while(*ptr != '\0')
+	 {
+      ITM_SendChar(*ptr);
+      ++ptr;
+   }
+	  sprintf(Msg, "%x", e);
+	 ptr = Msg ;
+   while(*ptr != '\0')
+	 {
+      ITM_SendChar(*ptr);
+      ++ptr;
+   }*/
+}
